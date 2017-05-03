3b1112f25477a9a2e1070ab45dab94ea0bad26af
Tomasz Karczewski <karczewskit@tt.com.pl>
2017-05-03 20:21:59 +0200

    Added camera preview from some example

--- app/src/main/java/pl/com/imralav/magisternative23/CameraCheckActivity.java
+++ app/src/main/java/pl/com/imralav/magisternative23/CameraCheckActivity.java
@@ -2,21 +2,22 @@ package pl.com.imralav.magisternative23;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.GestureDetector;
 import android.view.View;
 
+import pl.com.imralav.magisternative23.camera.CameraCheckFragment;
 import pl.com.imralav.magisternative23.fling.FlingDirection;
-import pl.com.imralav.magisternative23.fling.FlingGestureListener;
 
 public class CameraCheckActivity extends MagNativeActivity {
-
-    private GestureDetector gestureDetector;
-    private FlingGestureListener flingGestureListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera_check);
+        if (null == savedInstanceState) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.container, CameraCheckFragment.newInstance())
+                    .commit();
+        }
     }
 
     @Override
